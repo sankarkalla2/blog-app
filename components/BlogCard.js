2d8aa1320e59d105691e6fa48cefc7914cb9e3ae
sankrkall2 <gowrisankarkalla4@gmail.com>
2023-04-07 12:15:08 +0530

    add all

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -12,8 +12,12 @@ export function BlogCard({title, coverPhoto, datePublished,slug}) {
       </Link>
       <h1>Hello world</h1>
       <h2>Hello world</h2>
-      <h2>another hello world</h1>
+      <h2>another hello world</h2>
       <h3>another pull  request</h3>
+      <h4>git checkout tutorial</h4>
+      <h5>git branchs</h5>
+      <h6>hello world</h6>
+      <p>paragraph</p>
     </div>
   )
 }
